e45b2a1e27e2ea2f6ec97c3d36b237710dc258a0
Miku Nagakawa <149560996+nagakawa39@users.noreply.github.com>
2025-03-08 15:23:59 +0900

    added github link

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Illuminate\View\ViewServiceProvider::class,
         /*
          * Application Service Providers...
          */
